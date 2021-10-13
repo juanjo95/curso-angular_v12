c41034dc585a8dcda6c2ae549fff3e500d4fcb63
Juan J Salgado <zallagox95@hotmail.com>
2021-10-13 18:03:17 -0500

    Finalizacion de la app de la calculadora

--- app-calculadora/src/app/app.component.ts
+++ app-calculadora/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-calculadora';
+
+  private titulo = 'Aplicación de Calculadora';
+  operandoA!:number
+  operandoB!:number
+  resultado!:number
+
+  public getTitulo():string{
+    return this.titulo
+  }
+
+  public sumar():void{
+    this.resultado = this.operandoA + this.operandoB
+  }
+
 }
