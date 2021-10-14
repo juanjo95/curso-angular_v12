28db37333f2baf3d42ba6faec6efd24862de1625
Juan J Salgado <zallagox95@hotmail.com>
2021-10-14 13:48:42 -0500

    Property Binding entre componentes Padre---->Hijo @input() (Pasar info entre comp. Padre a un comp. Hijo)- carperta listado-personas

--- listado-personas/src/app/persona/persona.component.ts
+++ listado-personas/src/app/persona/persona.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Persona } from '../persona.model';
 
 @Component({
   selector: 'app-persona',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persona.component.css']
 })
 export class PersonaComponent implements OnInit {
+
+  @Input() persona!:Persona
+  @Input() indice!:number
 
   constructor() { }
 
