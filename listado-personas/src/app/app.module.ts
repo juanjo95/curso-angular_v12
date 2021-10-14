ac9a7f771b7cc3dee07361833884c2abcee6cf1b
Juan J Salgado <zallagox95@hotmail.com>
2021-10-14 12:12:55 -0500

    Property Binding entre componentes - carperta listado-personas

--- listado-personas/src/app/app.module.ts
+++ listado-personas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule, FormsModule
