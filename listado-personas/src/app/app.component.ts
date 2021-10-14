9e03862add9a2ea4e15c35d22924645b8b52e1f2
Juan J Salgado <zallagox95@hotmail.com>
2021-10-14 12:07:18 -0500

    Directiva *ngFor - carperta listado-personas- Ejemplo Finalizado

--- listado-personas/src/app/app.component.ts
+++ listado-personas/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Persona } from './persona.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de personas'
+
+
+  /* Declaramos un arreglo de tipo Persona (persona.model.ts) */
+  personas:Persona[] = [new Persona("Juan","Salgado"),new Persona("Carlos","Perez")]
+
+  nombreInput!:string
+  apellidoInput!:string
+
+  public agregarPersona():void{
+
+    let persona1 = new Persona(this.nombreInput,this.apellidoInput)
+    this.personas.push(persona1)
+
+
+  }
+
 }
---
+++ listado-personas/src/app/persona.model.ts
@@ -0,0 +1,14 @@
+export class Persona{
+  /*nombre!:string
+  apellido!:string
+
+  constructor(nombre:string, apellido:string){
+    this.nombre = nombre
+    this.apellido = apellido
+  }
+  */
+
+  /* Sintaxis simplificada a la declaracion anterior*/
+  constructor(public nombre:string, public apellido:string){}
+
+}
