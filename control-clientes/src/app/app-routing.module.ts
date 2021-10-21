4cdaadd874f62b8e800d4d464ec75da780ba7897
Juan J Salgado <zallagox95@hotmail.com>
2021-10-21 16:39:43 -0500

    Proyecto CONTROL-CLIENTES - Configuracion de rutas (app-routing.module.ts)

--- control-clientes/src/app/app-routing.module.ts
+++ control-clientes/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { LoginComponent } from './../../../listado-personas/src/app/login/login.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TableroComponent },
+  { path: 'login', component:  LoginComponent },
+  { path: 'registrarse', component: RegistroComponent },
+  { path: 'configuracion', component: ConfiguracionComponent },
+  { path: 'cliente/editar/:id', component: EditarClienteComponent },
+  { path: '**', component: NoEncontradoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
